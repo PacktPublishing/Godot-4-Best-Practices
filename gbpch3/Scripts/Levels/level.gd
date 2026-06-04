adbf4437685f54945ea29d5b0f1b5feaa3de2793
GameDevJourney101 <justfuncoding4all@gmail.com>
2026-06-04 10:53:27 +0100

    Delete gbpch3 directory

--- gbpch3/Scripts/Levels/level.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	# Example: Instancing a modular sub-scene
-	var enemy = preload("res://Scenes/Characters/Enemies/goblin.tscn").instantiate()
-	add_child(enemy)
-	enemy.position = Vector2(115,333)
